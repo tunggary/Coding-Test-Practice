ec56422c28036367db8fbcc55e9e9c2575c49f07
Seong Yoon <gsy1656@gmail.com>
2023-01-15 17:44:43 +0900

    [Silver IV] Title: 요세푸스 문제, Time: 64 ms, Memory: 34080 KB -BaekjoonHub

--- 백준/Silver/1158. 요세푸스 문제/요세푸스 문제.py
+++ 백준/Silver/1158. 요세푸스 문제/요세푸스 문제.py
@@ -1,33 +1,16 @@
 import sys
+from collections import deque
 
 n,m = map(int, sys.stdin.readline().split())
-print("<", end="")
-nodes = dict()
-for i in range(1, n+1):
-  if i == 1:
-    nodes[i] = {"num": i, "left": n, "right": i + 1}
-  elif i == n:
-    nodes[i] = {"num": i, "left": i - 1, "right": 1}
-  else:
-    nodes[i] = {"num": i, "left": i - 1, "right": i + 1}
-    
-current = 1
-count = 0
+
+answer = []
+nodes = deque([i for i in range(1,n+1)])
+
 while True:
-  if count < m - 1:
-    count += 1
-    current = nodes[current]["right"]
-    continue
   if len(nodes) == 1:
-    print(nodes[current]["num"], end="")
+    answer.append(nodes.popleft())
     break
-  else:
-    print(nodes[current]["num"], end=", ")
-  count = 0
-  nodes[nodes[current]["left"]]["right"] = nodes[current]["right"]
-  nodes[nodes[current]["right"]]["left"] = nodes[current]["left"]
-  temp = nodes[current]["right"]
-  del nodes[current]
-  current = temp
+  nodes.rotate(-(m-1))
+  answer.append(nodes.popleft())
   
-print(">")
+print('<', ', '.join(map(str, answer)), '>', sep='')
