9ccafe29fb8c1d0063c5c8a8f25f8e98f795daa6
Seong Yoon <gsy1656@gmail.com>
2023-02-02 23:39:14 +0900

    [Silver II] Title: N번째 큰 수, Time: 956 ms, Memory: 33324 KB -BaekjoonHub

--- 백준/Silver/2075. N번째 큰 수/N번째 큰 수.py
+++ 백준/Silver/2075. N번째 큰 수/N번째 큰 수.py
@@ -1,38 +1,16 @@
 import sys
 from heapq import heappush, heappop
 
-n = int(sys.stdin.readline())
-# array = [list(map(int, sys.stdin.readline().split())) for _ in range(n)]
-# count = [n-1]*n
-
-# def find_max():
-#   max_value = 0
-#   max_index = 0
-#   for i in range(n):
-#     if count[i] < 0:
-#       continue
-#     if array[count[i]][i] > max_value:
-#       max_value = array[count[i]][i]
-#       max_index = i
-#   return max_index, max_value
-      
-
-# for i in range(n):
-#   max_index, max_value = find_max()
-#   count[max_index] -= 1
-#   if i == n-1:
-#     print(max_value)
-
+n = int(sys.stdin.readline().strip())
 heap = []
 
-for _ in range(n):
-  numbers = map(int, sys.stdin.readline().split())
-  for number in numbers:
+for i in range(n):
+  array = list(map(int, sys.stdin.readline().split()))
+  for num in array:
     if len(heap) < n:
-      heappush(heap, number)
-    else:
-      if heap[0] < number:
-        heappop(heap)
-        heappush(heap, number)
-        
+      heappush(heap, num)
+    elif num > heap[0]:
+      heappush(heap, num)
+      heappop(heap)
+      
 print(heap[0])
