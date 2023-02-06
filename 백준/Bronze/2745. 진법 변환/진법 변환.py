98b0738cc4c33657862e98c89ad0179757bfda50
Seong Yoon <gsy1656@gmail.com>
2023-02-06 15:55:04 +0900

    [Bronze II] Title: 진법 변환, Time: 44 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Bronze/2745. 진법 변환/진법 변환.py
+++ 백준/Bronze/2745. 진법 변환/진법 변환.py
@@ -1,16 +1,5 @@
 import sys
 
 number, base = sys.stdin.readline().strip().split()
-base = int(base)
-answer = 0
 
-BOUNDARY = 10 - ord('A')
-length = len(number) - 1 
-
-for i in range(len(number)):
-  if number[length - i].isalpha():
-    answer += (base**i)*(ord(number[length - i]) + BOUNDARY)
-  else:
-    answer += (base**i)*int(number[length - i])
-
-print(answer)
+print(int(number, int(base)))
