d1a0429725e1ccbb308b7f5206ff5a2f1fee8271
Seong Yoon <gsy1656@gmail.com>
2023-02-07 16:23:39 +0900

    [Silver III] Title: 주유소, Time: 116 ms, Memory: 47204 KB -BaekjoonHub

--- 백준/Silver/13305. 주유소/주유소.py
+++ 백준/Silver/13305. 주유소/주유소.py
@@ -1,19 +1,15 @@
 import sys
 
 n = int(sys.stdin.readline().strip())
-distances = list(map(int, sys.stdin.readline().strip().split()))
-prices = [ (price, i) for i, price  in enumerate(map(int, sys.stdin.readline().strip().split()[:-1]))]
+dists = list(map(int, sys.stdin.readline().strip().split()))
+prices = list(map(int, sys.stdin.readline().strip().split()))
 
-prices.sort()
 answer = 0
-min_index = n
-min_price = 1000000000
-for price, index in prices:
-  if index > min_index:
-    answer += min_price * distances[index]
-    continue
-  min_price = price
-  min_index = index
-  answer += min_price * distances[index]
+min = prices[0]
+
+for i in range(n-1):
+  if prices[i] < min:
+    min = prices[i]
+  answer += min * dists[i]
   
 print(answer)
