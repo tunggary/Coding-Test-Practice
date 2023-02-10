a1dbd4f57bab9fd13cb1a37d6c6e80f4cc10cd12
Seong Yoon <gsy1656@gmail.com>
2023-02-10 11:29:38 +0900

    [Silver V] Title: 다리 놓기, Time: 40 ms, Memory: 33376 KB -BaekjoonHub

--- 백준/Silver/1010. 다리 놓기/다리 놓기.py
+++ 백준/Silver/1010. 다리 놓기/다리 놓기.py
@@ -1,20 +1,11 @@
 import sys
+from math import factorial
 
 t = int(sys.stdin.readline().strip())
 
 def solve():
   n, m = map(int, sys.stdin.readline().strip().split())
-  dp = [[0] * (m+1) for _ in range(n+1)]
-  
-  for i in range(1, m+1):
-    dp[1][i] = i
-    
-  for i in range(2, n+1):
-    for j in range(i, m+1):
-      for k in range(j, i-1, -1):
-        dp[i][j] += dp[i-1][k-1]
-
-  print(dp[n][m])
+  print(int(factorial(m) / (factorial(n)*factorial(m-n))))
 
 for _ in range(t):
   solve()
