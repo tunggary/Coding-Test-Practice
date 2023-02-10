dc3a5e87c0fd47b131ccd1eb554a8a429ed4531f
Seong Yoon <gsy1656@gmail.com>
2023-02-10 11:19:35 +0900

    [Silver V] Title: 다리 놓기, Time: 100 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Silver/1010. 다리 놓기/다리 놓기.py
+++ 백준/Silver/1010. 다리 놓기/다리 놓기.py
@@ -6,15 +6,14 @@ def solve():
   n, m = map(int, sys.stdin.readline().strip().split())
   dp = [[0] * (m+1) for _ in range(n+1)]
   
-  for i in range(1, n+1):
+  for i in range(1, m+1):
+    dp[1][i] = i
+    
+  for i in range(2, n+1):
     for j in range(i, m+1):
-      if i > j:
-        continue
-      if i == 1:
-        dp[i][j] = j
-      else:
-        for k in range(j, i-1, -1):
-          dp[i][j] += dp[i-1][k-1]
+      for k in range(j, i-1, -1):
+        dp[i][j] += dp[i-1][k-1]
+
   print(dp[n][m])
 
 for _ in range(t):
