370d885642da1112c13ca282aef7ecab1e288867
Seong Yoon <gsy1656@gmail.com>
2023-02-11 15:37:30 +0900

    [Silver III] Title: 조합, Time: 40 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Silver/2407. 조합/조합.py
+++ 백준/Silver/2407. 조합/조합.py
@@ -1,6 +1,16 @@
 import sys
-import math
+
+dp = [0]*101
+
+def factorial(n):
+  if n == 0:
+    return 1
+  elif dp[n] != 0:
+    return dp[n]
+  else:
+    dp[n] = n*factorial(n-1)
+    return dp[n]
 
 n,m = map(int,sys.stdin.readline().strip().split())
 
-print(math.factorial(n)//(math.factorial(m)*math.factorial(n-m)))
+print(factorial(n)//(factorial(m)*factorial(n-m)))
