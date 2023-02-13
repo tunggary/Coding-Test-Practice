0225df4c8f03ef9960fba475f90585946a2160bb
Seong Yoon <gsy1656@gmail.com>
2023-02-13 15:01:22 +0900

    [Silver II] Title: 가장 긴 짝수 연속한 부분 수열 (small), Time: 1632 ms, Memory: 234824 KB -BaekjoonHub

---
+++ 백준/Silver/22857. 가장 긴 짝수 연속한 부분 수열 （small）/가장 긴 짝수 연속한 부분 수열 （small）.py
@@ -0,0 +1,14 @@
+import sys
+
+n,k = map(int, sys.stdin.readline().strip().split())
+array = list(map(int, sys.stdin.readline().strip().split()))
+array = [0] + array
+dp = [[0]*(k+1) for _ in range(n+1)]
+
+for i in range(1, n+1):
+  for j in range(k+1):
+    if array[i] % 2 == 0:
+      dp[i][j] = dp[i-1][j] + 1
+    if array[i] % 2 == 1 and j > 0:
+      dp[i][j] = dp[i-1][j-1]
+print(max([dp[i][k] for i in range(1,n+1)]))
