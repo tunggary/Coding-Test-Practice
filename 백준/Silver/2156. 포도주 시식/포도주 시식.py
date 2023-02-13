6f7238ce21adeb27bcaaeaf790b46f6701c1e8c4
Seong Yoon <gsy1656@gmail.com>
2023-02-13 20:12:17 +0900

    [Silver I] Title: 포도주 시식, Time: 52 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Silver/2156. 포도주 시식/포도주 시식.py
+++ 백준/Silver/2156. 포도주 시식/포도주 시식.py
@@ -6,11 +6,11 @@ dp = [0]*n
 
 for i in range(n):
   if i == 0:
-    dp[i] = wines[i]
+    dp[i] = wines[0]
   elif i == 1:
-    dp[i] = wines[i-1] + wines[i]
+    dp[i] = wines[0]+wines[1]
   elif i == 2:
-    dp[i] = max(wines[i-2]+wines[i], wines[i-1]+wines[i], wines[i-1]+wines[i-2])
+    dp[i] = max(wines[0]+wines[1], wines[0]+wines[2], wines[1]+wines[2])
   else:
     dp[i] = max(dp[i-2]+wines[i], dp[i-3]+wines[i-1]+wines[i], dp[i-1]) 
   
