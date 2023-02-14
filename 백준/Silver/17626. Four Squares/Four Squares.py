3b9782e6d85fc177c552ea4c052ccbdfd2b81c8d
Seong Yoon <gsy1656@gmail.com>
2023-02-14 14:31:52 +0900

    [Silver III] Title: Four Squares, Time: 60 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Silver/17626. Four Squares/Four Squares.py
+++ 백준/Silver/17626. Four Squares/Four Squares.py
@@ -1,29 +1,26 @@
-n = int(input())
+import sys
 
-def check(n):
-  if is_sq[n] == True:
+n = int(sys.stdin.readline().strip())
+
+def is_square(num):
+  if num <= 0 or num != int(num**0.5)**2:
+    return False
+  else:
+    return True
+
+def find(num):
+  if is_square(num):
     return 1
-  m = len(sq)
-  for i in range(m):
-    if sq[i] > n:
-      break
-    if is_sq[n-sq[i]]:
+  
+  for i in range(1, int(num**0.5)+1):
+    if is_square(num-i**2):
       return 2
     
-  for i in range(m):
-    for j in range(i, m):
-      if sq[i] + sq[j] > n:
-        break
-      if is_sq[n-sq[i]-sq[j]]:
+  for i in range(1, int(num**0.5)+1):
+    for j in range(1, int(num**0.5)+1):
+      if is_square(num - i**2 - j**2):
         return 3
+      
   return 4
 
-is_sq = [False]*50001
-sq = []
-i=1
-while i*i<=50000:
-  is_sq[i*i] = True
-  sq.append(i*i)
-  i+=1
-  
-print(check(n))
+print(find(n))
