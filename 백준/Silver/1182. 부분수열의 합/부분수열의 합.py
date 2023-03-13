89f7049a523df7475ffd82406fd8171a4eba7cf1
Seong Yoon <gsy1656@gmail.com>
2023-03-13 17:56:21 +0900

    [Silver II] Title: 부분수열의 합, Time: 320 ms, Memory: 31256 KB -BaekjoonHub

---
+++ 백준/Silver/1182. 부분수열의 합/부분수열의 합.py
@@ -0,0 +1,17 @@
+import sys
+
+n,s = map(int, sys.stdin.readline().split())
+array = list(map(int, sys.stdin.readline().split()))
+array.sort()
+answer = 0
+
+def backtracking(sum, idx):
+  global answer
+  if sum == s:
+    answer += 1
+
+  for i in range(idx, n):
+    backtracking(sum+array[i], i+1)
+
+backtracking(0, 0)
+print(answer-1 if s == 0 else answer)
