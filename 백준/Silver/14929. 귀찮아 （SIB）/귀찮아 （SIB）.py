17e53f804f76971ee9c9c5df5d0aa84d39982b6d
Seong Yoon <gsy1656@gmail.com>
2023-03-21 09:01:13 +0900

    [Silver V] Title: 귀찮아 (SIB), Time: 92 ms, Memory: 42340 KB -BaekjoonHub

--- 백준/Silver/14929. 귀찮아 （SIB）/귀찮아 （SIB）.py
+++ 백준/Silver/14929. 귀찮아 （SIB）/귀찮아 （SIB）.py
@@ -2,15 +2,10 @@ import sys
 
 n = int(sys.stdin.readline())
 array = list(map(int, sys.stdin.readline().split()))
-
-culmulative_sum = [0]*n
-for i in range(n):
-  if i == 0:
-    culmulative_sum[i] = sum(array)
-  else:
-    culmulative_sum[i] = culmulative_sum[i-1] - array[i-1]
-  
 answer = 0
-for i in range(n-1):
-  answer += array[i] * culmulative_sum[i+1]
+
+total = sum(array)
+for i in range(n):
+  total -= array[i]
+  answer += array[i] * total
 print(answer)
