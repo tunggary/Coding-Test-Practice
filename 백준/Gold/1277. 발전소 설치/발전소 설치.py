9d49e44c782bcb3c0cf66b3545c30d9a31831d12
Seong Yoon <gsy1656@gmail.com>
2023-03-30 16:57:59 +0900

    [Gold IV] Title: 발전소 설치, Time: 1220 ms, Memory: 163708 KB -BaekjoonHub

---
+++ 백준/Gold/1277. 발전소 설치/발전소 설치.py
@@ -0,0 +1,44 @@
+import sys
+from heapq import heappush, heappop
+from math import floor
+
+n,w = map(int, sys.stdin.readline().split())
+m = float(sys.stdin.readline())
+position = [tuple(map(int, sys.stdin.readline().split())) for _ in range(n)]
+current = set([tuple(map(int, sys.stdin.readline().split())) for _ in range(w)])
+graph = [[] for _ in range(n+1)]
+INF = 1e9
+distance = [INF]*(n+1)
+
+for i in range(1, n+1):
+  for j in range(1, n+1):
+    i_x, i_y = position[i-1]
+    j_x, j_y = position[j-1]
+    dist = ((i_x - j_x)**2 + (i_y - j_y)**2)**0.5
+    
+    # if i == j or dist > m:
+    #   continue
+    
+    if (i, j) in current or (j, i) in current:
+      graph[i].append((j, 0))
+    else:
+      graph[i].append((j, dist))
+      
+def dijkstra(start):
+  distance[start] = 0
+  q = []
+  heappush(q, (0, start))
+  
+  while q:
+    dist, now = heappop(q)
+    if dist > distance[now]:
+      continue
+    for i in graph[now]:
+      cost = dist + i[1]
+      if cost < distance[i[0]]:
+        distance[i[0]] = cost
+        heappush(q, (cost, i[0]))
+dijkstra(1)
+      
+print(floor(distance[n]*1000))
+    
