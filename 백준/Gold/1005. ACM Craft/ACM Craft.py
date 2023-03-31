c082aabb20b41667bbcec022dd690e66da9259d1
Seong Yoon <gsy1656@gmail.com>
2023-03-31 16:41:44 +0900

    [Gold III] Title: ACM Craft, Time: 1004 ms, Memory: 34528 KB -BaekjoonHub

---
+++ 백준/Gold/1005. ACM Craft/ACM Craft.py
@@ -0,0 +1,38 @@
+import sys
+from collections import deque
+
+t = int(sys.stdin.readline())
+
+def solve():
+  n,k = map(int, sys.stdin.readline().split())
+  craft_time = [0] + list(map(int, sys.stdin.readline().split()))
+  indegree = [0]*(n+1)
+  graph = [[] for _ in range(n+1)]  
+  for _ in range(k):
+    a,b = map(int, sys.stdin.readline().split())
+    graph[a].append(b)
+    indegree[b] += 1
+  building_number = int(sys.stdin.readline())
+  dp = [0]*(n+1)
+  
+  if indegree[building_number] == 0:
+    return craft_time[building_number]
+  
+  q = deque()
+  for i in range(1, n+1):
+    dp[i] = craft_time[i]
+    if indegree[i] == 0:
+      q.append(i)
+  
+  while q:
+    now = q.popleft()
+    for i in graph[now]:
+      indegree[i] -= 1
+      dp[i] = max(dp[now] + craft_time[i], dp[i])
+      if indegree[i] == 0:
+        q.append(i)
+        
+  return dp[building_number]
+  
+for _ in range(t):
+  print(solve())
