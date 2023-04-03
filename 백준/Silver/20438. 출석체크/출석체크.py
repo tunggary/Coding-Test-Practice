9d562491cd709ccaadf9260012506c67196756c9
Seong Yoon <gsy1656@gmail.com>
2023-04-03 16:35:42 +0900

    [Silver II] Title: 출석체크, Time: 116 ms, Memory: 39880 KB -BaekjoonHub

--- 백준/Silver/20438. 출석체크/출석체크.py
+++ 백준/Silver/20438. 출석체크/출석체크.py
@@ -2,21 +2,20 @@ import sys
 
 n,k,q,m = map(int, sys.stdin.readline().split())
 sleeps = set(map(int, sys.stdin.readline().split()))
-codes = list(map(int, sys.stdin.readline().split()))
-queries = [list(map(int, sys.stdin.readline().split())) for _ in range(m)]
-check = [0] * (n+3)
-sum_array = [0] * (n+3)
+check = list(map(int, sys.stdin.readline().split()))
+commands = [list(map(int, sys.stdin.readline().split())) for _ in range(m)]
+presents = [1]*(n+3)
 
-for code in codes:
-  if code in sleeps:
+for student in check:
+  if student in sleeps:
     continue
-  for i in range(code, n+3, code):
+  for i in range(student, n+3, student):
     if i in sleeps:
       continue
-    check[i] = 1
+    presents[i] = 0
+    
+for i in range(1, n+3):
+  presents[i] += presents[i-1]
 
-for i in range(3, n+3):
-  sum_array[i] = sum_array[i-1] +  (1 if check[i] == 0 else 0)
-      
-for query in queries:
-  print(sum_array[query[1]] - sum_array[query[0]-1])
+for start, end in commands:
+  print(presents[end] - presents[start-1])
