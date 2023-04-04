c511a11f93097b20403520a1b42228a40bde4091
Seong Yoon <gsy1656@gmail.com>
2023-04-04 13:32:49 +0900

    [Gold II] Title: 공항, Time: 164 ms, Memory: 40532 KB -BaekjoonHub

---
+++ 백준/Gold/10775. 공항/공항.py
@@ -0,0 +1,34 @@
+import sys
+
+n = int(sys.stdin.readline())
+g = int(sys.stdin.readline())
+parents = [i for i in range(n+1)]
+entrance = [0]*(n+1)
+counts = [1]*(n+1)
+
+def find_parent(x):
+  if parents[x] != x:
+    parents[x] = find_parent(parents[x])
+  return parents[x]
+
+def union_parent(a, b):
+  a = find_parent(a)
+  b = find_parent(b)
+  if a < b:
+    parents[b] = a
+    counts[a] += counts[b]
+  else:
+    parents[a] = b
+    counts[b] += counts[a]
+    
+def set_entrance(num):
+  num = find_parent(num)
+  entrance[num] = 1
+  union_parent(num, num-1)
+
+for _ in range(g):
+  num = int(sys.stdin.readline())
+  if find_parent(num) == 0:
+    break
+  set_entrance(num)
+print(sum(entrance[1:]))
